30f0432fd34b639cf1159f7f2b933b7775439175
chigari101 <chigari101@gmail.com>
2022-09-10 10:13:34 -0700

    commit the files created

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
+ *
  * Return: Always 0 (success)
  */
 int main(void)
@@ -15,6 +16,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n" sizeof(d));
 	return (0);
 }
