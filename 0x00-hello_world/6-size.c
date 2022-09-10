ed56474c3feae0df48a87db1963215a37e261917
chigari101 <chigari101@gmail.com>
2022-09-10 11:21:12 -0700

    commit the files created

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	long int l;
-	long long int ll;
-	float d;
-	char c;
+	int inttype;
+	float floattype;
+	double doubletype;
+	char chartype;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
-	printf("Size of a float: %d byte(s)\n" sizeof(d));
+	printf("Size of int: %zu bytes\n", sizeof(inttype));
+	printf("Size of float: %zu bytes\n", sizeof(floattype));
+	printf("Size of double: %zu bytes\n", sizeof(doubletype));
+	printf("Size of char: %zu byte\n", sizeof(chartype));
+
 	return (0);
 }
