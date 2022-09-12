8e7c2cc8a423328c11e9aab96246323fe6818e80
chigari101 <chigari101@gmail.com>
2022-09-12 10:22:44 -0700

    commit the files created

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,23 @@ int main(void)
 
 	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (num2 = num2 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num2 % 10) + '0');
-			putchar(' ');
+			putchar(32);
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			if (num1 / 10 !=9 || num1 % 10 != 8)
+			{
+			putchar(44);
+			putchar(32);
+			}
 		}
 	}
 
-	putchar('\n');
+	putchar(10);
 
 	return (0);
 }
