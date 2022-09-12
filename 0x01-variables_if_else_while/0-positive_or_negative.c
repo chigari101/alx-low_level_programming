27666c0764be6a55b57fd3a65552d91703cc9a61
chigari101 <chigari101@gmail.com>
2022-09-12 04:21:43 -0700

    commit the file created

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdio.h>
 
 /**
  * main - Prints a random number and display whether
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
