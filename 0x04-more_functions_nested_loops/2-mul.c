4fe8c534e0cfa9b4b5d9bdc4ed6f2880a445bf27
chigari101 <chigari101@gmail.com>
2022-09-16 04:23:49 -0700

    commit the new files created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that multiply two numbers
+ * @a: integer parameter1
+ * @b: integer parameter2
+ * Return: success and integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints the numbersfrom 1 to 100, followed by new line.
+ * however for multiples of three points Fizz instead of the numbers.
+ * and for the multiples of five prints Buzz
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d" i);
+		}
+	}
+	printf('\n');
+
+	return (0);
+}
