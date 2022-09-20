8e9ce487fa27ecc4ae274aca81f127a76ce8890a
chigari101 <chigari101@gmail.com>
2022-09-19 22:29:48 -0700

    commit the file created

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,22 @@
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-
-		{
-			printf(" Fizz");
-		}
-		else if (i % 3 != 0 && i % 5 == 0)
-		{
-			printf(" Buzz");
-		}
+		if (i == 100)
+			printf("%s", b);
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
 		else
-		{
-			printf(" %d" i);
-		}
+			printf("%d ", i);
 	}
 	printf("\n");
 
