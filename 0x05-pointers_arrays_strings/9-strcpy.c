a6d14b8e517f28ed71a2120207e56523fa5e64e7
chigari101 <chigari101@gmail.com>
2022-09-19 23:45:30 -0700

    add the files created

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	for (index = 0 src[index] != '\0'; index++)
-		else if
-		{
-			dest[index] = src[index];
-		}
-		else
-		dest[index] = '\0';
-
-		return (dest);
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+	return (dest);
 }
