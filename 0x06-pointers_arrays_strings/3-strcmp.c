c76a3d6e205948f93e9ed25ea6e749915dc5a122
chigari101 <chigari101@gmail.com>
2022-09-23 12:56:54 -0700

    added files

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,11 +5,14 @@
  * @s1: pointer for the first string
  * @s2: pointer for the second string
  *
- * Return: int
+ * Return: if str1 < str2, negative difference
+ * if str1 == str2, 0
+ * if str1 > str2 positive difference.
  */
 int _strcmp(char *s1, char *s2)
 {
 	while ((*s1 && *s2 && *s1 == *s2)
+
 	{
 		s1++;
 		s2++;
