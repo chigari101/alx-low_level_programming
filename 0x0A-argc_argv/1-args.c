39ae7e1676480b932b912607893673f35b023cb8
chigari101 <chigari101@gmail.com>
2022-10-02 15:59:20 -0700

    added files C-argc nd argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
