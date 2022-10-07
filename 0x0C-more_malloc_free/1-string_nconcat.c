70fc06d8956acff80468bb5e06e71d3d6f5454b8
chigari101 <chigari101@gmail.com>
2022-10-07 03:24:59 -0700

    added files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,8 +12,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
 	unsigned int i, len1 = 0, len2 = 0;
+	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc(len1 + n + 1);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0; i < len1 + 1; i++)
 	{
 		if (*s1)
 		{
