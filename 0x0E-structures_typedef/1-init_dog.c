438665caf6c5630bb45b1b07714b13d8fcd18b7e
chigari101 <chigari101@gmail.com>
2022-10-11 16:42:58 -0700

    added files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog.
+ * @d: dog to be initialized
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner.
+ */
+void init_dog(struct dog *d, char *name, int age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+My readme file on C-structures nd typedef.
