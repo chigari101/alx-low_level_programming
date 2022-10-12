71203547487eba327e44bb70cbf194a33b42d53c
chigari101 <chigari101@gmail.com>
2022-10-12 15:29:33 -0700

    added files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#include <stdlib.h>
-
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 
-int _putchar(char c);
+#include <stdio.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
