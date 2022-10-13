cd489db1bd0950f6672b03b6560e00df8516634d
chigari101 <chigari101@gmail.com>
2022-10-13 02:08:58 -0700

    added files

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,15 +5,20 @@
  * on each element of an array
  * @array: An array of paramters (values) to be passed to @action
  * @size: Size/length of @array
- * @action: Pointer to a function
+ * @cmp: Pointer to a function
+ *
+ * Return: if no element matches return -1 otherwise return 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	size_t i;
+	int i;
 
-	if (!action || !array)
-		return;
-
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
-		action(array[i]);
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
