f47f545e3e4e53f4bf5adda174a2618a0b1f2f5b
chigari101 <chigari101@gmail.com>
2022-10-21 15:22:20 -0700

    added linked list files

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+	extern printf
+
+	SECTION .text
+	global main
+main;
+mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
+
+SECTION .data
+	msg:	db "Hello, Holberton", 0
+	fmt:	db "%s", 10, 0
