fc7be65311501be3728399dd12ae427e1ec0cd8f
chigari101 <chigari101@gmail.com>
2022-11-02 16:18:13 -0700

    added files created on file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/uio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string whose the length will be returned.
- * Return: The length of s.
- */
-int _strlen(char *s)
-{
-	if (*s == 0)
-		return (0);
-	else
-		return (1 + _strlen(s + 1));
-}
-
-/**
- * create_file - Creates a file.
- * @filename: The filename
- * @text_content: The content to write in the file.
- * Return: 1 on success. -1 otherwise.
+ * create_file - A function that creates a file
+ * @filename: The filename to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 on success, -1 if file can not be created, nor written,
+ * nor write fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = _strlen(text_content);
+	int fdo, fdw, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
-		write(fd, text_content, len);
-	return (len);
 
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
+		return (-1);
+
+	while (text_content && *(text_content + len))
+		len++;
+
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
+		return (-1);
+	return (1);
 }
